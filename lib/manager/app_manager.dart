3e3076eaa724079304de4785285bfe8afafb0ce8
Hyy800 <74134814+Hyy800@users.noreply.github.com>
2026-08-12 01:44:05 +0800

    fix: import shared glass surface

--- lib/manager/app_manager.dart
+++ lib/manager/app_manager.dart
@@ -7,6 +7,7 @@ import 'package:fl_clash/manager/window_manager.dart';
 import 'package:fl_clash/models/common.dart';
 import 'package:fl_clash/providers/providers.dart';
 import 'package:fl_clash/state.dart';
+import 'package:fl_clash/widgets/widgets.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
